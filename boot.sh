f1b3b86d626583eb059f66743c288b803c19a4c1
Thibault Clérice <leponteineptique@gmail.com>
2019-02-07 12:25:21 +0100

    Fix timeout for the moment

--- boot.sh
+++ boot.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec gunicorn -b :5000 --access-logfile - --error-logfile - flaskapp:app
+# Timeout of 60sec right now
+exec gunicorn -b :5000 --access-logfile - --error-logfile - flaskapp:app --timeout 60
